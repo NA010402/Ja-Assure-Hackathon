f8347046fdb57cb3902953f7d8ac270c56c4c669
ar7775 <ar7775@srmist.edu.in>
2023-04-18 14:59:01 +0530

    Complete

--- client_ret/src/components/Navbar.jsx
+++ client_ret/src/components/Navbar.jsx
@@ -4,11 +4,6 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../../images/logo.png";
 
-const NavBarItem = ({ title, classprops }) => (
-
-  <a href={`http://localhost:4000/${title}`} className={`mx-4 cursor-pointer ${classprops}`}>{title}</a>
-);
-
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
 
@@ -18,9 +13,12 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["New Purchase", "Retrieve", "Search"].map((item, index) => (
+        {/* {["New Purchase", "Retrieve", "Digital Gold"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
-        ))}
+        ))} */}
+        <a href="http://localhost:3000/" class="mx-4 cursor-pointer undefined">New Purchase</a>
+        <a href="http://localhost:4000/" class="mx-4 cursor-pointer undefined">Retrieve</a>
+        <a href="http://localhost:5000/" class="mx-4 cursor-pointer undefined">Digital Gold</a>
         <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
           Login
         </li>
@@ -38,9 +36,10 @@ const Navbar = () => {
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["New Purchase", "Retrieve", "Search"].map(
-              (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
-            )}
+            <a href={"http://localhost:3000/"} className="`mx-4 cursor-pointer my-2 text-lg">New Purchase</a>
+            <a href={"http://localhost:4000/"} className="`mx-4 cursor-pointer my-2 text-lg">Retrieve</a>
+            <a href={"http://localhost:5000/"} className="`mx-4 cursor-pointer my-2 text-lg">Digital Gold</a>
+
           </ul>
         )}
       </div>
